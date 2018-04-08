0b9e83317e2a9197e9e2af2d3e1b8f28e01a802c
Mike Green <mike.is.green@gmail.com>
2018-04-08 17:56:17 -0400

    Fire next middleware function after logging message

--- scripts/message-log.js
+++ scripts/message-log.js
@@ -2,5 +2,6 @@ module.exports = (robot) => {
   if (!process.env.HUBOT_LOG_MESSAGES) return;
   robot.listenerMiddleware((ctx, next, done) => {
     robot.logger.info(`${context.response.message.user.name}: ${context.response.message.text}`);
+    next();
   });
 };
