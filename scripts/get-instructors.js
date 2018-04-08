854144861ebc46d2fc4b578f36c72d14e9877a21
Mike Green <mike.is.green@gmail.com>
2018-04-08 17:47:23 -0400

    Storing instructor objects in Urbot's persistent storage once they've been loaded once, making message logging optional and off by default

--- scripts/get-instructors.js
+++ scripts/get-instructors.js
@@ -5,15 +5,22 @@ module.exports = (robot) => {
   }
 
   const { HUBOT_URBOT_INSTRUCTORS: defaultInstructors = '' } = process.env;
-  const instructors = robot.brain.get('instructors') || defaultInstructors.split(',');
 
   async function filterUsers(fn) {
     const { members: users } = await robot.adapter.client.web.users.list();
     return users.filter(fn);
   }
 
+  async function getInstructors() {
+    let instructors = robot.brain.get('instructors');
+    if (instructors) return instructors;
+    instructors = await filterUsers((usr) => defaultInstructors.split(',').includes(usr.name));
+    robot.brain.set('instructors', instructors);
+    return instructors;
+  }
+
   robot.hear(/@urbot instructors/i, async (res) => {
-    const users = await filterUsers((usr) => instructors.includes(usr.name));
+    const users = await getInstructors();
     const response = users.map((user) => `Name: ${user.real_name}, Username: ${user.name}`).join('\n');
     res.send(response);
   });
--- scripts/message-log.js
+++ scripts/message-log.js
@@ -1,3 +1,4 @@
 module.exports = (robot) => {
+  if (!process.env.HUBOT_LOG_MESSAGES) return;
   robot.respond(/.*/, (res) => robot.logger.info(res.message));
 };
