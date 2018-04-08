269e4043f5df7a9cd3f767721b293cba2a59a4b3
Mike Green <mike.is.green@gmail.com>
2018-04-08 17:55:41 -0400

    Change message logger to a listener middleware

--- scripts/message-log.js
+++ scripts/message-log.js
@@ -1,4 +1,6 @@
 module.exports = (robot) => {
   if (!process.env.HUBOT_LOG_MESSAGES) return;
-  robot.respond(/.*/, (res) => robot.logger.info(res.message));
+  robot.listenerMiddleware((ctx, next, done) => {
+    robot.logger.info(`${context.response.message.user.name}: ${context.response.message.text}`);
+  });
 };
