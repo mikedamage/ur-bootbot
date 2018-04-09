2c296658da83f6eae944f6130901b85a6ab24114
Mike Green <mike.is.green@gmail.com>
2018-04-09 16:27:05 -0400

    Add functions and ACLs for teaching assistants, add admins to all auth function whitelists

--- scripts/access-control.js
+++ scripts/access-control.js
@@ -1,14 +1,28 @@
 module.exports = (robot) => {
+  function isInstructor(user) {
+    return robot.auth.isAdmin(user) || robot.auth.hasRole(user, 'instructor');
+  }
+
+  function isInstructionalTeam(user) {
+    return isInstructor(user) || robot.auth.hasRole(user, 'assistant');
+  }
+
   robot.listenerMiddleware((ctx, next, done) => {
     const { name: userName } = ctx.response.message.user;
     const user = robot.brain.userForName(userName);
 
-    if ((/^instructor\./).test(ctx.listener.options.id) && !robot.auth.hasRole(user, 'instructor')) {
+    if ((/^instructor\./).test(ctx.listener.options.id) && !isInstructor(user)) {
       robot.logger.warn(`${user.real_name} tried to run an instructor command without authorization.`);
       ctx.response.reply(`I can't let you do that, Dave.`);
       done();
     }
 
-    next();
+    if ((/^assistant\./).test(ctx.listener.options.id) && !isInstructionalTeam(user)) {
+      robot.logger.warn(`${user.real_name} tried to run an assistant command without authorization.`);
+      ctx.response.reply(`Where'd you get this awful fake ID? I'm not buying it.`);
+      done();
+    }
+
+    next(done);
   });
 };
