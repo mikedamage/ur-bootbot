9f6c9536e9af9825d6134de260733ae30d288586
Mike Green <mike.is.green@gmail.com>
2018-04-09 11:07:16 -0400

    Stub access control middleware, add skeleton commands for adding instructors and TAs

--- scripts/get-instructors.js
+++ scripts/get-instructors.js
@@ -1,28 +1,34 @@
+const SlackHelpers = require('../lib/slack-helpers');
+
 module.exports = (robot) => {
   if (robot.adapterName !== 'slack') {
     robot.logger.warn('Not using slack adapter. Not initializing instructor commands.');
     return;
   }
 
+  const slack = new SlackHelpers(robot);
   const { HUBOT_URBOT_INSTRUCTORS: defaultInstructors = '' } = process.env;
-
-  async function filterUsers(fn) {
-    const { members: users } = await robot.adapter.client.web.users.list();
-    return users.filter(fn);
-  }
 
   async function getInstructors() {
     let instructors = robot.brain.get('instructors');
     if (instructors) return instructors;
-    instructors = await filterUsers((usr) => defaultInstructors.split(',').includes(usr.name));
+    instructors = await slack.filterUsers((usr) => defaultInstructors.split(',').includes(usr.name));
     robot.brain.set('instructors', instructors);
     return instructors;
   }
 
-  robot.hear(/@urbot instructors/i, async (res) => {
+  robot.hear(/@urbot instructors/i, { id: 'instructors.list' } async (res) => {
     const users = await getInstructors();
     const response = users.map((user) => `Name: ${user.real_name}, Username: ${user.name}`).join('\n');
     res.send(response);
+  });
+
+  robot.respond(/instructors add (.+)/, { id: 'instructors.add' }, async (res) => {
+
+  });
+
+  robot.respond(/assistants add (.+)/, { id: 'assistants.add' }, async (res) => {
+
   });
 };
 
