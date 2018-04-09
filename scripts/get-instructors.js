925c60b58f82932281f7811666232859a0a944ff
Mike Green <mike.is.green@gmail.com>
2018-04-09 14:25:52 -0400

    Rename env variable containing instructors. Need to replace auth script with something more Slack-friendly

--- scripts/get-instructors.js
+++ scripts/get-instructors.js
@@ -1,23 +1,20 @@
-const SlackHelpers = require('../lib/slack-helpers');
-
 module.exports = (robot) => {
   if (robot.adapterName !== 'slack') {
     robot.logger.warn('Not using slack adapter. Not initializing instructor commands.');
     return;
   }
 
-  const slack = new SlackHelpers(robot);
-  const { HUBOT_URBOT_INSTRUCTORS: defaultInstructors = '' } = process.env;
+  const { HUBOT_INSTRUCTORS: defaultInstructors = '' } = process.env;
 
   async function getInstructors() {
     let instructors = robot.brain.get('instructors');
     if (instructors) return instructors;
-    instructors = await slack.filterUsers((usr) => defaultInstructors.split(',').includes(usr.name));
+    instructors = defaultInstructors.split(',').map((username) => robot.brain.userForName(username));
     robot.brain.set('instructors', instructors);
     return instructors;
   }
 
-  robot.hear(/@urbot instructors/i, { id: 'instructors.list' } async (res) => {
+  robot.hear(/@urbot instructors/i, { id: 'instructors.list' }, async (res) => {
     const users = await getInstructors();
     const response = users.map((user) => `Name: ${user.real_name}, Username: ${user.name}`).join('\n');
     res.send(response);
